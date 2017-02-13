0a9a3ac2647c64520eb3e68d851ed57a908157fd
Roy Wei <roywjy@gmail.com>
2017-02-13 18:02:16 +0800

    slice updates 2

--- 18_slice/12_multi-dimentional/04_comparing_shorthand_var_make/02_var-slice/main.go
+++ 18_slice/12_multi-dimentional/04_comparing_shorthand_var_make/02_var-slice/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var student []string
+	var student []string // var will always set it to zero value
 	var students [][]string
 	student[0] = "Roy"
 	// student = append(student, "Roy")
--- 18_slice/13_int-slice-plus-plus/main.go
+++ 18_slice/13_int-slice-plus-plus/main.go
@@ -6,6 +6,7 @@ func main() {
 	mySlice := make([]int, 1)
 	fmt.Println(mySlice[0])
 	mySlice[0] = 7
+	// mySlice = append(mySlice, 8)
 	fmt.Println(mySlice[0])
 	mySlice[0]++
 	fmt.Println(mySlice[0])
