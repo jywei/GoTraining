81b81d76d9e52dbb8ac07aedd131b05cf942f1bb
Roy Wei <roywjy@gmail.com>
2017-02-23 17:57:26 +0800

    mutual exclusion object

---
+++ 22_go-routines/07_mutex/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var counter int
+var mutex sync.Mutex // mutual exclusion object
+
+func main() {
+	wg.Add(2)
+	go incrementor("Foo: ")
+	go incrementor("Bar: ")
+	wg.Wait()
+	fmt.Println("Final Counter: ", counter)
+}
+
+func incrementor(s string) {
+	for i := 0; i < 20; i++ {
+		time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+		mutex.Lock() // lock it, everything else holds and waits
+		counter++
+		// x := counter
+		// x++
+		// counter = x
+		fmt.Println(s, i, "Counter: ", counter)
+		mutex.Unlock()
+	}
+	wg.Done()
+}
+
+// go run -race main.go
+// vs
+// go run main.go
