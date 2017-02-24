c950c2ad6b883f9278b659c2fa3abe8a9061a9ce
Roy Wei <roywjy@gmail.com>
2017-02-24 15:59:10 +0800

    unbuffered channel

--- 22_go-routines/09_channels/00_unbuffered-channels-block/main.go
+++ 22_go-routines/09_channels/00_unbuffered-channels-block/main.go
@@ -6,19 +6,23 @@ import (
 )
 
 func main() {
-	c := make(chan int)
-
+	// channel is like map, so we use make for 'maps', 'slices', and 'channels'
+	c := make(chan int) // the unbuffered channel we will communicate with int
+	// Unbuffered channel is the channel don't explicitly assign size ie. cha := make(chan int, 10) => cha is a buffered channel
+	// two independently running go-routines
 	go func() {
 		for i := 0; i < 10; i++ {
-			c <- 1
+			c <- i // put i on the channel, then the code blocks
 		}
 	}()
 
 	go func() {
 		for {
-			fmt.Println(<-c)
+			fmt.Println(<-c) // we take the number out of the channel, then the code continues
 		}
 	}()
 
-	time.Sleep(time.Second)
+	time.Sleep(time.Second) // this will give time to go-routines to execute
 }
+
+// go run -race main.go
