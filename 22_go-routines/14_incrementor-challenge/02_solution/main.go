15b4d90ad10adb2c27795cb3f98a1211eea4a32a
Roy Wei <roywjy@gmail.com>
2017-03-19 00:31:37 +0800

    incrementor solution updates

--- 22_go-routines/14_incrementor-challenge/02_solution/main.go
+++ 22_go-routines/14_incrementor-challenge/02_solution/main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 
 	c := incrementor(2)
-
 	var count int
-	for n := range c {
+
+	for n := range c { // pull things out of the channel here
 		count++
 		fmt.Println(n)
 	}
@@ -25,13 +25,13 @@ func incrementor(n int) chan string {
 	for i := 0; i < n; i++ {
 		go func(i int) {
 			for k := 0; k < 20; k++ {
-				c <- fmt.Sprint("Process: "+strconv.Itoa(i)+" printing:", k)
-			}
+				c <- fmt.Sprint("Process: "+strconv.Itoa(i)+" printing:", k) // put things into the channel here
+			} // Itoa == int to ASCII
 			done <- true
 		}(i)
 	}
 
-	go func() {
+	go func() { // block until done channel is true, and close c channel
 		for i := 0; i < n; i++ {
 			<-done
 		}
