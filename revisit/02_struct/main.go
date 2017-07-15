439fcf6f50c8e593eef5bf17e77b189befbc0b92
Roy Wei <roywjy@gmail.com>
2017-07-15 17:08:06 +0800

    Add interface

--- revisit/02_struct/main.go
+++ revisit/02_struct/main.go
@@ -20,12 +20,21 @@ func (sa secretAgent) speak() {
 	fmt.Println(sa.fname, sa.lname, `says, "Shaken, not stirred."`)
 }
 
+type human interface { // If you have a speak method you are using a human interface
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
 func main() {
 	p1 := person{
 		"Miss",
 		"Moneypenny",
 	}
-	p1.speak()
+	// p1.speak()
+	saySomething(p1) // polymorphism
 	// fmt.Println(p1)
 
 	sa1 := secretAgent{
@@ -35,7 +44,8 @@ func main() {
 		},
 		true,
 	}
-	sa1.speak()
-	sa1.person.speak()
+	// sa1.speak()
+	// sa1.person.speak()
+	saySomething(sa1)
 	// fmt.Println(sa1)
 }
